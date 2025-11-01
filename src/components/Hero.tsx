474e093eb0fd99fc18abb52ba3f7e95199574193
Cristelle <cristelleal@gmail.com>
2025-11-01 08:37:20 +0100

    try to fix logo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,11 @@
+import CA from "../data/CA.png";
+
 const Hero = () => {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 px-2 md:px-8 py-8">
       <div className="flex justify-between items-start">
         <img
-          src="/public/CA.png"
+          src={CA}
           alt="CA logo"
           className="inline-block w-40 h-40 -mt-14 -ml-8"
         />
